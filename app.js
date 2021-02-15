ce96dbf5f796ad95186847d12ced3b4e2c6dd277
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-02-15 13:17:26 -0700

    Repo Init

---
+++ app.js
@@ -0,0 +1,8 @@
+// NOTE: Requirements
+// Part 1:
+// TODO: Create a new animal class for every letter of the alphabet.
+// Part 2:
+// TODO: Create at least one instance of every animal class
+// Part 3:
+// TODO: Group like animals in collections. Utilize array methods, e.g., .push()
+//       Hint: how could you utilize a new class, Pen, or Cage, to accomplish Part 3?
